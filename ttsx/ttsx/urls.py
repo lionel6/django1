879fc1629dc5ac9a0fec17927f72a3b47c2b5d00
lionel6 <qyliang6@163.com@163.com>
2017-10-14 20:09:53 +0800

    修改urls

--- ttsx/ttsx/urls.py
+++ ttsx/ttsx/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url('^goods', include('ttsx_user.urls'))
+    url('^goods', include('ttsx_goods.urls'))
 ]
